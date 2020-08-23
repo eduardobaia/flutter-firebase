f913a813cfe5ff4506e10b1465fc9d46e297f605
eduardobaia <eduardo_baia@msn.com>
2020-08-23 16:08:53 +0100

    add slider to swith the tile of sugar

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,72 @@
+import 'package:brew_crew/shared/constants.dart';
+import 'package:flutter/material.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4', '5'];
+
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Update your brew (coffe) settings.',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(height: 20.0),
+          TextFormField(
+            decoration: textInputDecoration,
+            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+            onChanged: (val) => setState(() => _currentName = val),
+          ),
+          SizedBox(height: 20.0),
+          //drobwond
+          DropdownButtonFormField(
+              decoration: textInputDecoration,
+              value: _currentSugars ?? '0',
+              items: sugars.map((sugar) {
+                return DropdownMenuItem(
+                  value: sugar,
+                  child: Text('$sugar sugars'),
+                );
+              }).toList(),
+              onChanged: (val) => setState(() => _currentSugars = val)
+              // onChanged: null,
+              ),
+          //slider
+          Slider(
+            value: (_currentStrength ?? 100).toDouble(),
+            min: 100.0,
+            max: 900.0,
+            divisions: 8,
+            onChanged: (val) => setState(() => _currentStrength = val.round()),
+          ),
+          //button
+          RaisedButton(
+            color: Colors.pink[400],
+            child: Text(
+              'Update',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              print(_currentName);
+              print(_currentStrength);
+              print(_currentSugars);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
